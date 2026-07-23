c3e42abd4c96b370c905ce7b4a508f6f2164110f
guhan0000 <guhanm0000@gmail.com>
2026-07-23 11:47:35 +0530

    useReducer completed

--- src/UseReducer/LoginDemo.jsx
+++ src/UseReducer/LoginDemo.jsx
@@ -1,19 +1,21 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 function reducer(state, action) {
   const { type, payload } = action;
   switch (type) {
     case "LOGIN_SUCCESS":
       return { ...state, user: payload };
     case "LOGIN_FAILURE":
-      return { ...state, error: payload };
+      return { ...state, user: null, error: payload };
     default:
       return state;
   }
 }
 const LoginDemo = () => {
   const initialState = { loading: false, user: null, error: "" };
-  const [state, dispatch] = useReducer(useReducer, initialState);
-
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
   return (
     <>
       <div>LoginDemo</div>
@@ -22,9 +24,19 @@ const LoginDemo = () => {
       <button
         onClick={() => {
           dispatch({ type: "LOGIN_SUCCESS", payload: "Guhan" });
+          // console.log(state);
         }}
       >
         login
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch({ type: "LOGIN_FAILURE", payload: "Invalid Credentials" });
+          // console.log(state);
+        }}
+      >
+        Fail
       </button>
     </>
   );
