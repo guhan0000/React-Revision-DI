b76467729b1311a834c2bc73859f9efd48290c7c
guhan0000 <guhanm0000@gmail.com>
2026-07-23 14:30:18 +0530

    app modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import NavBar from "./Routing/NavBar";
-import Home from "./Routing/pages/Home";
-import About from "./Routing/pages/About";
-import Contact from "./Routing/pages/Contact";
-import Movie from "./Routing/pages/Movie";
-import Student from "./Routing/pages/Student";
-import Team from "./Routing/pages/Team";
-import Leadership from "./Routing/pages/Leadership";
-import Locations from "./Routing/pages/Locations";
-import ServiceContacts from "./Routing/pages/ServiceContacts";
-import Login from "./Routing/pages/Login";
-import Logout from "./Routing/pages/Logout";
+import { AuthProvider } from "./UseReducer-with-UseContext/AuthContext";
+import Login from "./UseReducer-with-UseContext/Login";
+import Profile from "./UseReducer-with-UseContext/Profile";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />}>
-          <Route path="team" element={<Team />}></Route>
-          <Route path="leadership" element={<Leadership />}></Route>
-        </Route>
-        {/*Nested Routes*/}
-        <Route path="/contact" element={<Contact />}>
-          <Route index element={<ServiceContacts />} />
-          <Route path="locations" element={<Locations />} />
-        </Route>
-        <Route path="*" element={<h1>Page Not Found</h1>} />
-        <Route path="/movie/:id" element={<Movie />} /> {/*url parameters*/}
-        <Route path="/student/:id/:name" element={<Student />} />{" "}
-        {/*multiple parameters*/}
-        <Route path="/logout" element={<Logout />}></Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<h2>Page not found</h2>} />
+        </Routes>
+      </AuthProvider>
     </>
   );
 }
