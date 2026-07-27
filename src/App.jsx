ce6521ca609864e12488ad690623cff20146ea48
guhan0000 <guhanm0000@gmail.com>
2026-07-27 12:22:28 +0530

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import { AuthProvider } from "./UseReducer-with-UseContext/AuthContext";
-import Login from "./UseReducer-with-UseContext/Login";
-import Profile from "./UseReducer-with-UseContext/Profile";
+import CounterParent from "./UseCallback/CounterParent";
+
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="*" element={<h2>Page not found</h2>} />
-        </Routes>
-      </AuthProvider>
+    <CounterParent/>
     </>
   );
 }
