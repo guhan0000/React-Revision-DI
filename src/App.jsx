f0cd9f1a688a4ed30b75fdd774e102c9758457bd
guhan0000 <guhanm0000@gmail.com>
2026-07-28 15:22:42 +0530

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import Dashboard from "./CustomHooks/Dashboard";
-
-
+import React from "react";
+import { lazy } from "react";
+import { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./lazy-loading/pages/Navbar";
+const Home = lazy(() => import("./lazy-loading/pages/Home"));
+const About = lazy(() => import("./lazy-loading/pages/About"));
+const Contact = lazy(() => import("./lazy-loading/pages/Contact"));
 function App() {
   return (
     <>
-     <Dashboard/>
+      <Navbar />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
