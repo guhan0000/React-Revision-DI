921dbfd790e9283dbd69e8452bf0ecb14083c812
guhan0000 <guhanm0000@gmail.com>
2026-07-28 15:42:24 +0530

    lazy loading with button trigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { lazy } from "react";
 import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./lazy-loading/pages/Navbar";
 const Home = lazy(() => import("./lazy-loading/pages/Home"));
 const About = lazy(() => import("./lazy-loading/pages/About"));
+const Dashboard = lazy(() => import("./CustomHooks/Dashboard"));
 const Contact = lazy(() => import("./lazy-loading/pages/Contact"));
+
 function App() {
+  const [show, setShow] = useState(false);
   return (
     <>
       <Navbar />
@@ -17,6 +20,18 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Suspense>
+      {show && (
+        <Suspense fallback={<h1>Loading Products...</h1>}>
+          <Dashboard />
+        </Suspense>
+      )}
+      <button
+        onClick={() => {
+          setShow(true);
+        }}
+      >
+        Load
+      </button>
     </>
   );
 }
