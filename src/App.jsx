65f7ebd313d52d7451c8946457b12fdf624fbb0e
guhan0000 <guhanm0000@gmail.com>
2026-08-10 15:30:12 +0530

    app changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./lazy-loading/pages/Navbar";
 import SortProducts from "./dummyJSON/SortProducts";
 import FilterProducts from "./dummyJSON/FilterProducts";
+import CartOperations from "./dummyJSON/CartOperations";
 const Home = lazy(() => import("./lazy-loading/pages/Home"));
 const About = lazy(() => import("./lazy-loading/pages/About"));
 const Dashboard = lazy(() => import("./CustomHooks/Dashboard"));
@@ -13,29 +14,32 @@ const Contact = lazy(() => import("./lazy-loading/pages/Contact"));
 function App() {
   const [show, setShow] = useState(false);
   return (
+    // <>
+    //   <Navbar />
+    //   <Suspense fallback={<h1>Loading...</h1>}>
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/about" element={<About />} />
+    //       <Route path="/contact" element={<Contact />} />
+    //     </Routes>
+    //   </Suspense>
+    //   {show && (
+    //     <Suspense fallback={<h1>Loading Products...</h1>}>
+    //       <Dashboard />
+    //     </Suspense>
+    //   )}
+    //   <button
+    //     onClick={() => {
+    //       setShow(true);
+    //     }}
+    //   >
+    //     Load
+    //   </button>
+    //   {/* <SortProducts /> */}
+    //   {/* <FilterProducts /> */}
+    // </>
     <>
-      <Navbar />
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Suspense>
-      {show && (
-        <Suspense fallback={<h1>Loading Products...</h1>}>
-          <Dashboard />
-        </Suspense>
-      )}
-      <button
-        onClick={() => {
-          setShow(true);
-        }}
-      >
-        Load
-      </button>
-      {/* <SortProducts /> */}
-      <FilterProducts />
+      <CartOperations />
     </>
   );
 }
