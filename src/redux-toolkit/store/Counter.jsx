f786b1168958350b410c1e51fc30790ba3975ff4
guhan0000 <guhanm0000@gmail.com>
2026-08-18 17:34:56 +0530

    reset added

--- src/redux-toolkit/store/Counter.jsx
+++ src/redux-toolkit/store/Counter.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   increment,
   decrement,
   incrementByAmount,
+  reset,
 } from "./slice/counter/counterSlice";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  useEffect(() => {
+    console.log("render");
+  }, [count]);
   return (
     <div>
       <h4>Counter</h4>
@@ -33,6 +37,13 @@ const Counter = () => {
         }}
       >
         +5
+      </button>
+      <button
+        onClick={() => {
+          dispatch(reset());
+        }}
+      >
+        reset
       </button>
     </div>
   );
--- src/redux-toolkit/store/slice/counter/counterSlice.js
+++ src/redux-toolkit/store/slice/counter/counterSlice.js
@@ -12,8 +12,12 @@ const counterSlice = createSlice({
     incrementByAmount: (state, action) => {
       state.value += action.payload;
     },
+    reset: (state) => {
+      state.value = 0;
+    },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount, reset } =
+  counterSlice.actions;
 export default counterSlice.reducer;
