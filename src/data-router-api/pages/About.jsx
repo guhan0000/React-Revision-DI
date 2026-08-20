550423d02e6a4fb3fcf48f43fda4c034a30f8a70
guhan0000 <guhanm0000@gmail.com>
2026-08-20 12:52:15 +0530

    nested routing

--- src/data-router-api/pages/About.jsx
+++ src/data-router-api/pages/About.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const About = () => {
   return (
     <div>
       <h4>About</h4>
+      {/* crct*/}
+      <Link to={"team"}>Team</Link>
+      <br />
+      <Link to={"achievements"}>Achievements</Link>
+      <br />
+      {/* <Link to={"/history"}>History</Link>  wrong*/}
+      <Link to={"history"}>History</Link>
+      <Outlet />
     </div>
   );
 };
--- src/data-router-api/routes/routes.jsx
+++ src/data-router-api/routes/routes.jsx
@@ -1,12 +1,23 @@
 import About from "../pages/About";
+import Achievements from "../pages/Achievements";
 import Contact from "../pages/Contact";
+import History from "../pages/History";
 import Home from "../pages/Home";
 import Product from "../pages/Product";
 import ProductDetails from "../pages/ProductDetails";
+import Team from "../pages/Team";
 
 const routes = [
   { path: "/", element: <Home /> },
-  { path: "/about", element: <About /> },
+  {
+    path: "/about",
+    element: <About />,
+    children: [
+      { path: "team", element: <Team /> },
+      { path: "history", element: <History /> },
+      { path: "achievements", element: <Achievements /> },
+    ],
+  },
   { path: "/contact", element: <Contact /> },
   { path: "/product", element: <Product /> },
   { path: "/product/:id", element: <ProductDetails /> },
